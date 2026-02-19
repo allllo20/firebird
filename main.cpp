aaf1fed9b6d3814046200a7196c20f5a67240202
allllo20 <152682376+allllo20@users.noreply.github.com>
2026-02-19 16:22:09 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -19,24 +19,6 @@
 #include <sys/syscall.h>
 #endif
 
-#ifdef __EMSCRIPTEN__
-void initializeDefaultWasmKit() {
-    auto kit_model = the_qml_bridge->getKitModel();
-    
-    // Check if default WASM kit already exists
-    if (kit_model->rowCount() == 0) {
-        kit_model->addKit(
-            QStringLiteral("WebAssembly Default"),
-            QStringLiteral("/roms/boot1.img.tns"),
-            QStringLiteral("/roms/flash"),
-            QStringLiteral("/roms/snap")
-        );
-        
-        the_qml_bridge->setDefaultKit(0);
-    }
-}
-#endif
-
 
 /* This function reads all keys of all sections available in a QSettings
  * instance created with org and app as parameters. */
